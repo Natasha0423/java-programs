ad9b131943d950cb15b8d5e59fed9da4dc2c7d5e
Natasha0423 <65958893+Natasha0423@users.noreply.github.com>
2020-06-26 02:12:43 +0530

    Add files via upload

---
+++ java/ld/my_own_package2.java
@@ -0,0 +1,11 @@
+import Mypack.Mypack1.*;
+class my_own_package2{
+        public static void main(String s[]) 
+       {
+             sd ob= new sd();
+             ob.ps();
+             System.out.print("\n");
+            
+             
+        }
+}
---
+++ java/ld/natasha.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class natasha extends Applet{
+     public void paint(Graphics g){
+           // g.setColor(Color,RED);
+            //g.drawString("Natasha Sharma",150,150);
+            g.drawString("be positive",500,500);
+     }
+}
+/*
+  <applet code = "natasha.class" width="400" height="400">
+  </applet>
+*/
